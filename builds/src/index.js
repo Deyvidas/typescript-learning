f6d2a04e715cabb6de1d503f1bff99ee57833ddc
Deyvidas <devidbanyavichyus@yandex.ru>
2024-01-29 13:59:51 +0400

    Declared a function that makes it easier to get DOM element and check its type.

--- builds/src/index.js
+++ builds/src/index.js
@@ -1,45 +1,42 @@
+import { getDomElement } from "./utils.js";
 import { validateExpression } from "./validator.js";
 // TODO Make a report of errors in a separate area, not in the calculator screen.
 // TODO Place a value where the cursor is positioned.;
 const calcScreenId = "calculatorScreen";
+const calcScreenLoc = `input[id="${calcScreenId}"]`;
 const calcButtonId = "calculateButton";
 const eraseButtonId = "eraseButton";
+const calculatorScreen = getDomElement(calcScreenLoc);
+const calculator = getDomElement("table");
 let alreadyCalculated = false;
-const calculatorScreen = document.querySelector(`input[id="${calcScreenId}"]`);
-const calculator = document.querySelector("table");
-if (calculator !== null) {
-    calculator.onclick = (event) => {
-        let clickedElement;
-        if (!(event.target instanceof HTMLButtonElement)) {
-            return;
-        }
-        else if (calculatorScreen === null) {
-            return;
-        }
-        clickedElement = event.target;
-        if (clickedElement.id == calcScreenId) {
-            return;
-        }
-        else if (clickedElement.id == calcButtonId) {
-            calculatorScreen.value = calculateArithmeticalExpression(calculatorScreen.value);
-            alreadyCalculated = true;
-            return;
-        }
-        else if (clickedElement.id == eraseButtonId) {
-            calculatorScreen.value = calculatorScreen.value.slice(0, -1);
-            return;
-        }
-        else if (clickedElement.tagName != "BUTTON") {
-            return; //In that case if client make a click between two buttons
-        }
-        if (alreadyCalculated) {
-            calculatorScreen.value = "";
-            alreadyCalculated = false;
-        }
-        calculatorScreen.value += clickedElement.innerText;
+calculator.onclick = (event) => {
+    let clickedElement;
+    if (!(event.target instanceof HTMLButtonElement)) {
         return;
-    };
-}
+    }
+    clickedElement = event.target;
+    if (clickedElement.id == calcScreenId) {
+        return;
+    }
+    else if (clickedElement.id == calcButtonId) {
+        calculatorScreen.value = calculateArithmeticalExpression(calculatorScreen.value);
+        alreadyCalculated = true;
+        return;
+    }
+    else if (clickedElement.id == eraseButtonId) {
+        calculatorScreen.value = calculatorScreen.value.slice(0, -1);
+        return;
+    }
+    else if (clickedElement.tagName != "BUTTON") {
+        return; //In that case if client make a click between two buttons
+    }
+    if (alreadyCalculated) {
+        calculatorScreen.value = "";
+        alreadyCalculated = false;
+    }
+    calculatorScreen.value += clickedElement.innerText;
+    return;
+};
 function calculateArithmeticalExpression(expression) {
     try {
         return eval(validateExpression(expression));
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,45 @@
+import { getDomElement } from "./utils.js";
 import { validateExpression } from "./validator.js";
 // TODO Make a report of errors in a separate area, not in the calculator screen.
 // TODO Place a value where the cursor is positioned.;
 const calcScreenId: string = "calculatorScreen";
+const calcScreenLoc: string = `input[id="${calcScreenId}"]`;
+
 const calcButtonId: string = "calculateButton";
 const eraseButtonId: string = "eraseButton";
+
+const calculatorScreen = getDomElement<HTMLInputElement>(calcScreenLoc);
+const calculator = getDomElement<HTMLTableElement>("table");
+
 let alreadyCalculated: boolean = false;
 
-const calculatorScreen: HTMLInputElement | null = document.querySelector(
-    `input[id="${calcScreenId}"]`
-);
-const calculator: HTMLTableElement | null = document.querySelector("table");
-
-if (calculator !== null) {
-    calculator.onclick = (event) => {
-        let clickedElement: HTMLButtonElement;
-        if (!(event.target instanceof HTMLButtonElement)) {
-            return;
-        } else if (calculatorScreen === null) {
-            return;
-        }
-        clickedElement = event.target;
-
-        if (clickedElement.id == calcScreenId) {
-            return;
-        } else if (clickedElement.id == calcButtonId) {
-            calculatorScreen.value = calculateArithmeticalExpression(
-                calculatorScreen.value
-            );
-            alreadyCalculated = true;
-            return;
-        } else if (clickedElement.id == eraseButtonId) {
-            calculatorScreen.value = calculatorScreen.value.slice(0, -1);
-            return;
-        } else if (clickedElement.tagName != "BUTTON") {
-            return; //In that case if client make a click between two buttons
-        }
-
-        if (alreadyCalculated) {
-            calculatorScreen.value = "";
-            alreadyCalculated = false;
-        }
-        calculatorScreen.value += clickedElement.innerText;
+calculator.onclick = (event) => {
+    let clickedElement: HTMLButtonElement;
+    if (!(event.target instanceof HTMLButtonElement)) {
         return;
-    };
-}
+    }
+    clickedElement = event.target;
+
+    if (clickedElement.id == calcScreenId) {
+        return;
+    } else if (clickedElement.id == calcButtonId) {
+        calculatorScreen.value = calculateArithmeticalExpression(calculatorScreen.value);
+        alreadyCalculated = true;
+        return;
+    } else if (clickedElement.id == eraseButtonId) {
+        calculatorScreen.value = calculatorScreen.value.slice(0, -1);
+        return;
+    } else if (clickedElement.tagName != "BUTTON") {
+        return; //In that case if client make a click between two buttons
+    }
+
+    if (alreadyCalculated) {
+        calculatorScreen.value = "";
+        alreadyCalculated = false;
+    }
+    calculatorScreen.value += clickedElement.innerText;
+    return;
+};
 
 function calculateArithmeticalExpression(expression: string): string {
     try {
